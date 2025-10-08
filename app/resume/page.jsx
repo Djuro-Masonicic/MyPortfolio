d8c26579b09d6149cc44239172f94cce4641c594
djuro.masonicic <djuro.masonicic@S26001263L10>
2025-10-08 16:15:14 +0200

    resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -63,7 +63,7 @@ const experience = {
       company: "Freelancer",
       postion: "Full Stack Developer",
       duration: "2024 - Present",
-    },
+    }
   ],
 };
 
@@ -131,7 +131,28 @@ const Resume = ({}) => {
 
           <div className="min-h-[70vh] w-full">
             <TabsContent value="experience" className="w-full">
-              experience
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0" >{experience.description}</p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                  {experience.items.map((item,index)=>{
+                    return (
+                      <li key={index}
+                      className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl
+                      flex flex-col justify-center items-center lg:items-start gap-1">
+                        <span className="text-accent">{item.duration}</span>
+                        <h3>{item.postion}</h3>
+                        <div>
+                          <span></span>
+                          <p>{item.company}</p>
+                        </div>
+                      </li>
+                    )
+                  })}
+                  </ul>
+                </ScrollArea>
+              </div>
             </TabsContent>
             <TabsContent value="education" className="w-full">
               education
