5a3c7a76c1b420bcbd53c17db5f297eed9c8eef1
djuro.masonicic <djuro.masonicic@S26001263L10>
2025-10-14 15:54:40 +0200

    contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,52 @@
-const Contact = ({}) => {
-  return <div>contact page</div>
-}
+"use client";
 
-export default Contact
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "(+382) 67 444 884",
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    description: "djuromas@gmail.com",
+  },
+  {
+    icon: <FaMapMarkerAlt />,
+    title: "Address",
+    description: "Podgorica, Montenegro",
+  },
+];
+
+import { motion } from "framer-motion";
+
+const Contact = ({}) => {
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6">
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          <div className="xl:h-[54%]">
+            <form>form</form>
+          </div>
+          <div>info</div>
+        </div>
+      </div>
+    </motion.section>
+  );
+};
+
+export default Contact;
