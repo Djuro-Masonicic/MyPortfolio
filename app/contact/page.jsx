df9f5e6ae49af53256fe7c8d8a70e074e663079e
Djuro-Masonicic <djuromasonicic12345@gmail.com>
2025-10-14 20:51:21 +0200

    deploy

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -39,10 +39,22 @@ const Contact = ({}) => {
       className="py-6">
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
-          <div className="xl:h-[54%]">
-            <form>form</form>
+          <div className="xl:h-[54%] order-2 xl:order-none">
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl" >
+              <h3 className="text-4xl text-accent">Let's work together</h3>
+              <p className="text-white/60">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus nemo accusantium voluptates iste, molestias possimus consequatur cupiditate.
+              </p>
+
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                  <Input  type="firstname"  placeholder="Firstname"/>
+                  <Input  type="lastname"  placeholder="Lastname"/>
+                  <Input  type="email"  placeholder="Email adress"/>
+                  <Input  type="phone"  placeholder="Phone number"/>
+              </div>
+            </form>
           </div>
-          <div>info</div>
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">info</div>
         </div>
       </div>
     </motion.section>
